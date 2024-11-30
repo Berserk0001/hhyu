6cafcbb07271f6119839351cebef39c63f559a84
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-11-30 14:04:12 +0530

    Update proxy1.js

--- proxy1.js
+++ proxy1.js
@@ -79,7 +79,7 @@ function compress(req, res, input) {
     .pipe(
       sharpInstance
         .resize(null, 16383, {
-          //fit: 'inside',
+          fit: 'inside',
           withoutEnlargement: true
         })
         .grayscale(req.params.grayscale)
